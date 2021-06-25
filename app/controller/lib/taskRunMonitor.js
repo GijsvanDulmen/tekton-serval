a894f448156ed87c3ca139ae8363428a899ad67f
Gijs van Dulmen <gijs@vandulmen.net>
2021-06-25 11:01:14 +0200

    Add better examples and fix missing file

---
+++ app/controller/lib/taskRunMonitor.js
@@ -0,0 +1,59 @@
+const EventEmitter = require('events');
+
+module.exports = class TaskRunMonitor extends EventEmitter {
+    constructor() {
+        super();
+        this.taskruns = {};
+
+        this.on('created', taskRunKey => console.log(taskRunKey));
+        this.on('updated', (taskRunKey, newStatus) => console.log(taskRunKey + " = " + newStatus));
+    }
+
+    getReason(obj) {
+        if ( obj.status
+            && obj.status.conditions
+            && obj.status.conditions[0]
+            && obj.status.conditions[0].reason ) {
+
+            if ( obj.status.conditions[0].reason == 'Started'
+                || obj.status.conditions[0].reason == 'Pending'
+                || obj.status.conditions[0].reason == 'Running' ) {
+                return 'running'
+            } else if ( obj.status.conditions[0].reason == 'TaskRunCancelled' ) {
+                return 'cancelled';
+            } else if ( obj.status.conditions[0].reason == 'Failed' ) {
+                return 'failed';
+            } else if ( obj.status.conditions[0].reason == 'Succeeded' ) {
+                return 'succeeded';
+            }
+        }
+        return 'unknown';
+    }
+
+    processPipelineRun(obj) {
+        // if ( obj.status && obj.status.runs ) {
+        //     Object.keys(obj.status.runs).forEach(taskRunKey => {
+        //         console.log(obj.spec.pipelineSpec.tasks)
+        //         console.log(obj.status);
+        //     });
+        // }
+
+        if ( obj.status && obj.status.taskRuns ) {
+            Object.keys(obj.status.taskRuns).forEach(taskRunKey => {
+                console.log(this.taskruns[taskRunKey]);
+
+                if ( this.taskruns[taskRunKey] == undefined ) {
+                    this.taskruns[taskRunKey] = obj.status.taskRuns[taskRunKey];
+                    this.emit('created', taskRunKey);
+                } else {
+                    const oldStatus = this.getReason(this.taskruns[taskRunKey]);
+                    const newStatus = this.getReason(obj.status.taskRuns[taskRunKey]);
+                    if ( oldStatus != newStatus ) {
+                        this.taskruns[taskRunKey] = obj.status.taskRuns[taskRunKey];
+                        this.emit('updated', taskRunKey, newStatus, obj.status.taskRuns[taskRunKey], obj);
+                    }
+                }
+            });
+        }
+    }
+}
